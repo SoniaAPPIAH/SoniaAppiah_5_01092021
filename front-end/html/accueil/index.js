cbe5b3f5b05545a1d01584a8830f8a138d1dfdd5
Sonia Appiah <sonia.appiah1@gmail.com>
2021-09-20 03:40:22 +0200

    Fetch Index.html

--- front-end/html/accueil/index.js
+++ front-end/html/accueil/index.js
@@ -1,8 +1,27 @@
-fetch("http://localhost:3000/api/teddies")
-    .then( data => data.json())
-    .then( jsonListTeddy => {
+// --------------- PARAMETRES DES API --------------- //
 
-    
+let TeddiesAPI = 'http://localhost:3000/api/teddies';
+let TeddiesId = 'http://localhost:3000/api/teddies/:_id';
+let TeddiesOrder = 'http://localhost:3000/api/teddies/order';
+
+// --------------- CONNEXION AVEC l'API --------------- //
+
+class TeddiesCard{
+    constructor(jsonTeddiesCard){
+        this.jsonTeddiesCard = jsonTeddiesCard;
+    }
+}
+
+fetch(TeddiesAPI)
+    .then( data => data.json())
+    .then( jsonListTeddies => {
+        for(let jsonTeddies of jsonListTeddies){
+            let card = new TeddiesCard(jsonTeddies);
+            document.getElementsByClassName("Teddies-card")[0].innerHTML += ` <div id="template-card" class="justify-content-center text-center">
+                                                                                    <img class="photo" src=${jsonTeddies.imageUrl}/>
+                                                                                    <h2 class="title">${jsonTeddies.name}</h2>
+                                                                                    <p class="description">${jsonTeddies.description}</p>
+                                                                                    <p class="price">${jsonTeddies.price}</p>
+                                                                                </div>`
+        }
     });
-        
-   

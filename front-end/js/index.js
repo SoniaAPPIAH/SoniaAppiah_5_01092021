e6cf187f6112761098ec6d3dcf3123b02b95be93
Sonia Appiah <sonia.appiah1@gmail.com>
2021-09-25 01:13:18 +0200

    Product.js ID/Card

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -12,7 +12,6 @@ async function getTeddies () {
     .then( data => data.json())
     .then( jsonListTeddies => {
         createCards (jsonListTeddies);
-
     });
 }
 
@@ -20,40 +19,14 @@ async function getTeddies () {
 
 function createCards (jsonTeddies) {
     for(let jsonTeddy of jsonTeddies){
-        document.getElementsByClassName("Teddies-card")[0].innerHTML += `<a href="product.html" id="template-card" data-id="${jsonTeddy._id}" class="col-2 justify-content-center text-center">
-                                                                            <img class="photo" src="${jsonTeddy.imageUrl}"/>
-                                                                            <h2 class="title">${jsonTeddy.name}</h2>
-                                                                            <p class="description">${jsonTeddy.description}</p>
-                                                                            <p class="price">${jsonTeddy.price}</p>
+        document.getElementsByClassName("Teddies-card")[0].innerHTML += `<a href="product.html?_id=${jsonTeddy._id}" id="template-card" data-id="${jsonTeddy._id}" class="col-2 justify-content-center text-center">
+                                                                            <img class="photo_index" src="${jsonTeddy.imageUrl}"/>
+                                                                            <h2 class="title_index">${jsonTeddy.name}</h2>
+                                                                            <p class="description_index">${jsonTeddy.description}</p>
+                                                                            <p class="price_index">${jsonTeddy.price}</p>
                                                                          </a>`;
     }
     
 }
 
 getTeddies ();
-
-
-
-// ------------------------------ PAGE PRODUIT ------------------------------ //
-// ---- Récupération des informations de chaque Teddy ---- //
-
-// ---- Ajout des produits au panier---- //
-
-function addProduct(teddiesId) {
-    let listProducts = getProducts();
-    listProducts.push(teddiesId);
-    saveProducts(listProducts);
-}
-
-function getProducts(){
-    let listProducts = localStorage.getItem("listProducts");
-    if(listProducts == null) {
-        return [];
-    }else{
-        return JSON.parse(listProducts);
-    }
-}
-
-function saveProducts(listProducts){
-    localStorage.setItem("listProducts",JSON.stringify(listProducts));
-}
--- front-end/js/product.js
+++ front-end/js/product.js
@@ -1,23 +1,23 @@
 // ------------------------------ PAGE PRODUIT ------------------------------ //
 // ---- Récupération des informations de chaque Teddy ---- //
+let params = (new URL(document.location)).searchParams;
+let TeddyId = params.get("_id");
 
-// ---- Ajout des produits au panier---- //
+// ---- Personnalisation de l'adresse de l'API ---- //
+const TeddyUrl = `http://localhost:3000/api/teddies/${TeddyId}`;
 
-function addProduct(teddiesId) {
-    let listProducts = getProducts();
-    listProducts.push(teddiesId);
-    saveProducts(listProducts);
-}
+fetch(TeddyUrl)
+    .then(data => data.json())
+    .then((infoTeddy) => {
+        addCard(infoTeddy);
 
-function getProducts(){
-    let listProducts = localStorage.getItem("listProducts");
-    if(listProducts == null) {
-        return [];
-    }else{
-        return JSON.parse(listProducts);
+        function addCard(product) {
+            document.getElementsByClassName("Teddies-display")[0].innerHTML += `<a href="product.html?_id=${product._id}" id="template-card" data-id="${product._id}" class="col-2 justify-content-center text-center">
+                                                                                    <img class="photo_product" src="${product.imageUrl}"/>
+                                                                                    <h2 class="title_product">${product.name}</h2>
+                                                                                    <p class="description_product">${product.description}</p>
+                                                                                    <p class="price_product">${product.price}</p>
+                                                                                </a>`;
     }
-}
+});
 
-function saveProducts(listProducts){
-    localStorage.setItem("listProducts",JSON.stringify(listProducts));
-}

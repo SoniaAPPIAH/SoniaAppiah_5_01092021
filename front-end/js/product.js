afcde314c60fcc3435410fb490041a2ef77a028c
Sonia Appiah <sonia.appiah1@gmail.com>
2021-09-26 22:11:54 +0200

    Product.js Colors

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -6,18 +6,32 @@ let TeddyId = params.get("_id");
 // ---- Personnalisation de l'adresse de l'API ---- //
 const TeddyUrl = `http://localhost:3000/api/teddies/${TeddyId}`;
 
+
 fetch(TeddyUrl)
     .then(data => data.json())
     .then((infoTeddy) => {
         addCard(infoTeddy);
 
         function addCard(product) {
-            document.getElementsByClassName("Teddies-display")[0].innerHTML += `<a href="product.html?_id=${product._id}" id="template-card" data-id="${product._id}" class="col-2 justify-content-center text-center">
+            document.getElementsByClassName("Teddies-display")[0].innerHTML += `<div id="template-card_product" data-id="${product._id}">
                                                                                     <img class="photo_product" src="${product.imageUrl}"/>
-                                                                                    <h2 class="title_product">${product.name}</h2>
-                                                                                    <p class="description_product">${product.description}</p>
-                                                                                    <p class="price_product">${product.price}</p>
-                                                                                </a>`;
-    }
-});
+                                                                                    <div class="bloc_text_product">
+                                                                                        <h1 class="title_product">${product.name}</h1>
+                                                                                        <p class="description_product">${product.description}</p>
+                                                                                        <p class="price_product">${product.price}</p>
+                                                                                        <label>
+                                                                                            <select name="colors" class="Teddies-colors">
+                                                                                            </select>
+                                                                                        </label>
+                                                                                    </div>
+                                                                                </div>`;
+        addColors(product);                                                                                               
+        }
+
+        function addColors(product) {
+            for(let colors of product.colors) {
+                document.getElementsByClassName("Teddies-colors")[0].innerHTML += `<option>${colors}</option>`; 
+            }
+        }
+    });
 
